bf14b3570ac8c746dba6294578c0f479fef1456c
rarichardson92 <rrichard@uoregon.edu>
2018-10-11 13:20:53 -0700

    update

--- uobioinfo/main.py
+++ uobioinfo/main.py
@@ -2,7 +2,7 @@ import argparse
 
 
 def say_something(phrase): # ... I'm giving up on you. 
-    """Change this however you'd like!"""
+    """Change this however you'd like! NOOOO"""
     print(phrase)
 
 
